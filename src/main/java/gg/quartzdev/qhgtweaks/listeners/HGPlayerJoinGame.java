309073438543e58989ab93f50ce6934ae84abda1
QarthO <QarthO@gmail.com>
2023-08-14 11:27:33 -0400

    started debugging

--- src/main/java/gg/quartzdev/qhgtweaks/listeners/HGPlayerJoinGame.java
+++ src/main/java/gg/quartzdev/qhgtweaks/listeners/HGPlayerJoinGame.java
@@ -1,5 +1,6 @@
 package gg.quartzdev.qhgtweaks.listeners;
 
+import gg.quartzdev.qhgtweaks.util.Util;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -10,7 +11,8 @@ public class HGPlayerJoinGame implements Listener {
     @EventHandler
     public void onPlayerJoinGame(PlayerJoinGameEvent event){
         Player player = event.getPlayer();
-        String defaultKitName = event.getGame().getKitManager().getKitList().get(0);
-        event.getGame().getKitManager().setKit(player, defaultKitName);
+        Util.log("<aqua>" + player.getName() + " <green> joined the arena");
+//        String defaultKitName = event.getGame().getKitManager().getKitList().get(0);
+//        event.getGame().getKitManager().setKit(player, defaultKitName);
     }
 }
